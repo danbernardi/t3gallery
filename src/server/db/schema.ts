152174af3a3d3a4ba8e780d4bb696b9a8a5cfae6
Daniel Bernardi <danbernardi1@gmail.com>
2024-07-08 12:08:42 -0700

    Linked db to ui

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -18,11 +18,12 @@ import {
  */
 export const createTable = pgTableCreator((name) => `t3gallery_${name}`);
 
-export const posts = createTable(
-  "post",
+export const images = createTable(
+  "image",
   {
     id: serial("id").primaryKey(),
     name: varchar("name", { length: 256 }),
+    url: varchar('url', { length: 1024 }),
     createdAt: timestamp("created_at", { withTimezone: true })
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
